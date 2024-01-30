aab4f360c6f75a5df8919c2540449132195f7ac7
Bruno Aybar <miguel_bruno@hotmail.com>
2024-01-30 01:11:28 -0500

    feat: add navigation bar. mobile may not work properly

--- web/src/jsMain/kotlin/Main.kt
+++ web/src/jsMain/kotlin/Main.kt
@@ -1,18 +1,48 @@
 import kotlinx.browser.*
 import kotlinx.html.*
 import kotlinx.html.dom.*
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.org.w3c.dom.events.Event
+import org.w3c.dom.get
 
 fun main() {
     document.body!!.append.div {
-        h1 {
-            +"Welcome to Android Dev Peru!"
+        navBar()
+    }
+}
+
+fun DIV.navBar() {
+    nav {
+        id = "navbar"
+
+        img(src = "logo.svg", alt = "Logo") {
+            id = "logo"
+            height = "48px"
+            width = "48px"
         }
-        p {
-            +"Fancy joining this year's "
-            a("https://kotlinconf.com/") {
-                +"Android Dev Conf"
-            }
-            +"?"
+        span {
+            id = "nav-title"
+            +"Android Dev Peru"
         }
+        div {
+            id = "hamburger"
+            onClickFunction = ::toggleNav
+            +"☰"
+        }
+        ul {
+            li { a("#") { +"Eventos" } }
+            li { a("#") { +"Blog" } }
+            li { a("#") { +"Miembros" } }
+            li { a("#") { +"Contactanos" } }
+        }
+    }
+}
+
+fun toggleNav(event: Event) {
+    val nav = document.getElementById("navbar")!!
+    if (nav.classList.contains("open")) {
+        nav.classList.remove("open")
+    } else {
+        nav.classList.add("open")
     }
 }
