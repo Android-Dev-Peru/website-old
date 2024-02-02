b1d5e7c37c923eae3902ded3078cd5b003153d2d
Bruno Aybar <miguel_bruno@hotmail.com>
2024-02-01 21:33:45 -0500

    fix: broken linkedin and youtube links

--- web/src/jsMain/kotlin/data/Data.kt
+++ web/src/jsMain/kotlin/data/Data.kt
@@ -4,9 +4,9 @@ object Links {
     const val DevTo = "https://dev.to/androiddevperu"
     const val Twitter = "https://twitter.com/androiddevperu"
     const val GitHub = "https://github.com/Android-Dev-Peru"
-    const val LinkedIn = "https://www.linkedin.com/in/androiddevperu/"
+    const val LinkedIn = "https://www.linkedin.com/company/android-dev-peru"
     const val Instagram = "https://www.instagram.com/androiddevperu/"
-    const val YouTube = "https://www.youtube.com/channel/UC9Z3Xzvz3Kzg5vz5JL3J2jA"
+    const val YouTube = "https://www.youtube.com/@AndroidDevPeru"
     const val TikTok = "https://www.tiktok.com/@androiddevperu"
     const val WhatsApp = "https://chat.whatsapp.com/Il7yhDYCj8zLVDrK7OpFOm"
 }
